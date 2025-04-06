a439b563501c6b0f976a38c61b9baa464b8ca511
Symbol24 <phillip.conte@gmail.com>
2025-04-06 19:57:01 -0400

    new boot animations

--- Code/UI/boot.gd
+++ Code/UI/boot.gd
@@ -1,28 +1,33 @@
 class_name Boot extends Node2D
 
+
 const GAME = "res://Scenes/Utilities/game.tscn"
+
 
 @onready var animator: AnimationPlayer = %animator
 
+
 var step := 0
 var step_running := false
+var current := &"godot"
+
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("backward") or Input.is_action_just_pressed("forward"):
-		_skip()
+		_anim_check(current)
+
 
 func _ready() -> void:
 	animator.animation_finished.connect(_anim_check)
-	_start()
+	await get_tree().create_timer(1).timeout
+	animator.play(current)
 
-func _start() -> void:
-	animator.play("godot")
 
 func _anim_check(_anim_name:="") -> void:
-	if _anim_name == "godot": animator.play("logo")
-	elif _anim_name == "logo": S.LoadSceneFromPath.emit(GAME)
-
-func _skip() -> void:
-	if animator.current_animation == "godot": animator.play("logo")
-	elif animator.current_animation == "logo": S.LoadSceneFromPath.emit(GAME)
-		
+	if _anim_name == "godot": 
+		current = &"logo"
+		animator.play(&"RESET")
+	elif _anim_name == &"RESET":
+		animator.play(current)
+	elif _anim_name == "logo": 
+		S.LoadSceneFromPath.emit(GAME)
