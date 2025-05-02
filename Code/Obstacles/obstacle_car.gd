96ce21dad5403ec6351b4f0c2e02fe46b8465075
Symbol24 <phillip.conte@gmail.com>
2025-05-02 09:56:10 -0400

    im so bad at commits

--- Code/Obstacles/obstacle_car.gd
+++ Code/Obstacles/obstacle_car.gd
@@ -1,13 +1,16 @@
 class_name ObstacleCar extends StaticBody2D
 
+
 @export var car_colors:CarColorData
 
 @onready var outline: Sprite2D = %outline
 @onready var body: Sprite2D = %body
 @onready var windows: Sprite2D = %windows
 
+
 func _ready() -> void:
 	_set_car_color()
+
 
 func _set_car_color() -> void:
 	var metallic := randi_range(0,1)
